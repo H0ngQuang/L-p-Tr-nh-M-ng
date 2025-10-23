e244ae01cc69f501e27a3a0aae05e84e6d2fd6d6
H0ngQuang <hongquang02082004@gmail.com>
2025-10-23 17:39:50 +0700

    f

--- LTM/src/Task/Doi_chieu_bien_thien.java
+++ LTM/src/Task/Doi_chieu_bien_thien.java
@@ -19,5 +19,20 @@ public class Doi_chieu_bien_thien {
         DataOutputStream writer = new DataOutputStream (socket.getOutputStream());
         writer.writeUTF(code);
         writer.flush();
+        
+        String s =reader.readUTF();
+        ArrayList<Integer>a = new ArrayList<>();
+        String []tmp = s.trim().split(",");
+        for(String x: tmp) a.add(Integer.parseInt(x.trim()));
+        int dbt = 0;
+        int n = a.size();
+        for(int i=1;i<n;i++) dbt += Math.abs(a.get(i) - a.get(i-1));
+        int doichieu =0;
+        for(int i=1;i<n-1;i++){
+            if((a.get(i) > a.get(i-1) && a.get(i) > a.get(i+1)) || (a.get(i) < a.get(i-1) && a.get(i) < a.get(i+1))) doichieu++;
+        }
+        writer.writeInt(doichieu);
+        writer.writeInt(dbt);
+        writer.flush();
     }
 }
